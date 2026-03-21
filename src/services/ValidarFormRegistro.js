aae25268727411d9d722e18e7ab6481dece3e994
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2026-03-21 19:38:30 -0400

    fix: correcion de issues-> Uso incorrecto de optional chaining

--- src/services/ValidarFormRegistro.js
+++ src/services/ValidarFormRegistro.js
@@ -1,11 +1,11 @@
 import Mascota from '../domain/Mascota.js';
 
 export function validarFormulario() {
-  const nombre = (document.getElementById('nombre') || {}).value || '';
-  const especie = (document.getElementById('especie') || {}).value || '';
-  const edad = (document.getElementById('edad') || {}).value || '';
-  const raza = (document.getElementById('Raza') || {}).value || '';
-  const sexo = (document.getElementById('Sexo') || {}).value || '';
+  const nombre = document.getElementById('nombre')?.value || '';
+  const especie = document.getElementById('especie')?.value || '';
+  const edad = document.getElementById('edad')?.value || '';
+  const raza = document.getElementById('Raza')?.value || '';
+  const sexo = document.getElementById('Sexo')?.value || '';
 
   if (!nombre.trim() || !especie.trim() || !edad.toString().trim() || !raza.trim() || !sexo.trim()) {
     alert('Por favor, complete todos los campos del formulario.');
