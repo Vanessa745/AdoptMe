2f7cadab29922da070675fb185f9559e0f08af31
lucianaaaaaaa <luciana.leano@ucb.edu.bo>
2026-03-21 18:10:22 -0400

    fix: correcion de issues->Condición negada innecesaria

--- src/services/MarcarEstado.js
+++ src/services/MarcarEstado.js
@@ -1,9 +1,9 @@
 function MarcarEstado(estado){
-  if(estado !== "adoptado"){
+  if(estado === "adoptado"){
+    return"La mascota ha sido marcada como adoptado.";
+  } else {
     return"La mascota ha sido marcada como " + estado + " para adopción.";
-    } else {
-        return"La mascota ha sido marcada como adoptado.";
-    }
+  }
 }
 
 export {MarcarEstado};
